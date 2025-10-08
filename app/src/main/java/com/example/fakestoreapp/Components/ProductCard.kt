644b39360365e5c4edd54a60f4b6b91c4cb58751
Mariano Jara <mjaravillasana@gmail.com>
2025-10-07 22:56:22 -0600

    Toques finales, cambios en colores, estilización de product card y todo eso

--- app/src/main/java/com/example/fakestoreapp/Components/ProductCard.kt
+++ app/src/main/java/com/example/fakestoreapp/Components/ProductCard.kt
@@ -1,59 +1,98 @@
 package com.example.fakestoreapp.Components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import com.example.fakestoreapp.Models.Product
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 
 
 @Composable
 fun ProductCard(product: Product, onClick : () -> Unit){
-    Row (
+    Column  (
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
-            .height(100.dp)
-            .clip(CircleShape)
-            .background(Color.White)
+            .height(300.dp)
+            .shadow(30.dp, RoundedCornerShape(12.dp), clip = false)
+            .clip(RoundedCornerShape(12.dp))
+            .background(Color.Black)
             .padding(10.dp)
             .clickable{
                 onClick()
                       }
         ,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
 
     ){
         AsyncImage(
             model = product.image,
             contentDescription = product.title,
             modifier = Modifier
-                .size(80.dp)
-                .clip(CircleShape)
-                .background(Color.White)
+                .size(120.dp)
+                .padding(bottom = 10.dp)
+                .clip(RoundedCornerShape(6.dp))
+                .background(Color.LightGray)
+                .weight(2f)
         )
 
-        Column (
+        LazyColumn(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(start = 10.dp)
-                .weight(1f),
         ) {
-            Text(product.title)
-            Text(product.category)
+            item {
+                Text(
+                    text = product.title,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                )
+            }
+
+            item {
+                Text(
+                    text = product.category,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+                )
+            }
+
+            item {
+                Text(
+                    text = "$ ${product.price}",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Light,
+                        color = Color.White
+                    )
+                )
+            }
         }
-        Text(text = "$ ${product.price}")
+
 
 
 
--- app/src/main/java/com/example/fakestoreapp/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/fakestoreapp/Screens/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.example.fakestoreapp.Screens
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,6 +25,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -35,6 +41,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(navController: NavController){
@@ -79,38 +87,59 @@ fun HomeScreen(navController: NavController){
             CircularProgressIndicator()
         }
     } else {
-
-        LazyVerticalGrid (
-            columns = GridCells.Fixed (2),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-            contentPadding = PaddingValues(bottom = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(vertical = 30.dp)
         ) {
+            Text(
+                text = "Fake Store",
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Bold,
+                fontSize = 60.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+            Text(
+                text = "Popular fake products:",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .padding(16.dp)
 
-            item {
-                Text(text = "Fake Store",
-                    style = MaterialTheme.typography.headlineLarge,
-                    modifier = Modifier
-                        .padding(top = 40.dp)
+            )
+
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+                    .background(Color.LightGray)
+                    .padding(8.dp)
+                    ,
+                contentPadding = PaddingValues(bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                items(products) { product ->
+                    ProductCard(
+                        product,
+                        onClick = {
+                            navController.navigate(ProductDetailScreenRoute(product.id))
+                        }
+
                     )
-            }
+                }
 
-            items(products) { product ->
-                ProductCard(
-                    product,
-                    onClick = {
-                        navController.navigate(ProductDetailScreenRoute(product.id))
-                    }
-
-                )
             }
         }
+
+
     }
 }
+
 
 @Preview
 @Composable
